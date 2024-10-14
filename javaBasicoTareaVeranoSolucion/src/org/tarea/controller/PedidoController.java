27efa59742c73e18abe1884e0d80e9ef66e79c90
carmelo escribano <carmelo.escribano@gmail.com>
2024-10-14 16:57:23 +0200

    Add files via upload

--- javaBasicoTareaVeranoSolucion/src/org/tarea/controller/PedidoController.java
+++ javaBasicoTareaVeranoSolucion/src/org/tarea/controller/PedidoController.java
@@ -9,11 +9,13 @@ import java.util.Scanner;
 
 public class PedidoController {
     List<Pedido> pedidos = new ArrayList<>();
+   
     public void listarPedidos() {
         System.out.println("Listar pedidos");
        for(Pedido pedido: pedidos){
-              System.out.println(pedido);
-              System.out.println(pedido.getProducto());
+             // System.out.println(pedido);
+           System.out.println(pedido.getCodigo()+"-"+pedido.getFechaPedido()+"-"+pedido.getNombreCliente()+"-"+pedido.getProducto()+"-"+pedido.getUnidades()+"-"+pedido.getPrecio());
+
                 System.out.println("-----------------------");
        }
     }
@@ -33,15 +35,37 @@ public class PedidoController {
         double precio=scanner.nextDouble();
 
         Pedido pedido= new Pedido(codigo, new Date(), cliente, producto, unidades, precio);
-        pedidos.add(pedido);
-        System.out.println("Pedido añadido: "+pedido);
+        pedidos.add(pedido);//añadir pedido a la lista de pedidos
+        System.out.println("Pedido añadido: "+pedido.getCodigo());
     }
 
     public void modificarPedido() {
-        System.out.println("Modificar pedido");
+        System.out.println("Modificar pedido las unidades del pedido");
+        System.out.println("dime el código del pedido");
+        Scanner scanner = new Scanner(System.in);
+        int codigo= scanner.nextInt();
+        for(Pedido pedido: pedidos){
+            if(pedido.getCodigo()==codigo){
+                System.out.println("dime las nuevas unidades");
+                int unidades=scanner.nextInt();
+                pedido.setUnidades(unidades);
+                System.out.println("Pedido modificado: "+pedido.getCodigo());
+                break;
+            }
+        }
     }
 
     public void eliminarPedido() {
         System.out.println("Eliminar pedido");
+        System.out.println("dime el código del pedido");
+        Scanner scanner = new Scanner(System.in);
+        int codigo= scanner.nextInt();
+        for(Pedido pedido: pedidos){
+            if(pedido.getCodigo()==codigo){
+                pedidos.remove(pedido);
+                System.out.println("Pedido eliminado: "+pedido.getCodigo());
+                break;
+            }
+        }
     }
 }//cierra class PedidoController
